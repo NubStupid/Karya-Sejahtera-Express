0b659ca39bd1099a4cf4a84a16cfa649bc952e79
Stephanie Natasya <tasya315@gmail.com>
2024-10-30 11:08:34 +0700

    Navbar distributor

---
+++ components/distributor/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar()
+{
+    return (
+        <>
+            <div className="bg-orange-400 py-4 px-20 flex">
+                <div className="me-auto">
+                    LOGO
+                </div>
+                <div>
+                    <button className="bg-sky-300 py-1 px-2 rounded-md me-2">Back to homepage</button>
+                    profpic
+                </div>
+            </div>
+        </>
+    )
+}
