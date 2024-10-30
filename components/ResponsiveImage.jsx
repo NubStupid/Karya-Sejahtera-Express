1ed26ef4e702e8412164b65b4cdd08f7668f10a8
Ferdinand Susanto Halim <119311557+NubStupid@users.noreply.github.com>
2024-10-30 12:23:35 +0700

    Added 2 Responsive Component
    Responsive Component:
    1. Button
    2. Image

---
+++ components/ResponsiveImage.jsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import Box from '@mui/material/Box';
+
+const ResponsiveImage = ({ src, alt }) => {
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        // Define image width for various screen sizes
+        width: {
+          xs: '200%', // default for extra-small screens
+          '@media (min-width:600px)': {
+            width: '75%', // small screens
+          },
+          '@media (min-width:960px)': {
+            width: '60%', // medium screens
+          },
+          '@media (min-width:1280px)': {
+            width: '50%', // large screens
+          },
+          '@media (min-width:1920px)': {
+            width: '35%', // extra-large screens
+          }
+        },
+        height: 'auto' // to maintain aspect ratio
+      }}
+    >
+      <Image
+        src={src}
+        alt={alt}
+        layout="fill" // This makes the image fill the box
+        objectFit="contain" // Ensures the image fits within the box
+      />
+    </Box>
+  );
+};
+
+export default ResponsiveImage;
--- components/distributor/Navbar.jsx
+++ components/distributor/Navbar.jsx
@@ -1,16 +1,24 @@
+import { Avatar } from "@mui/material";
+import ResponsiveButton from "../ResponsiveButton";
+import ResponsiveImage from "../ResponsiveImage";
+
 export default function Navbar()
 {
     return (
         <>
-            <div className="bg-orange-400 py-4 px-20 flex">
-                <div className="me-auto">
-                    LOGO
-                </div>
-                <div>
-                    <button className="bg-sky-300 py-1 px-2 rounded-md me-2">Back to homepage</button>
-                    profpic
-                </div>
+           <div className="grid grid-cols-7 bg-orange-primary w-[100vw] p-5">
+            <div className="texl-xl flex justify-center">
+
+            <ResponsiveImage src="/logo/KDistributor.png" alt="KS Distributor"/>
+
             </div>
+            <div className="texl-xl col-start-6 flex justify-end ms-5">
+                <ResponsiveButton placeholder={"Back to Homepage"} href={"/"}/>
+            </div>
+            <div className="texl-xl flex justify-center">
+                <Avatar alt="Guest" src="" sx={{ width: 50, height: 50 }}/>
+            </div>
+        </div>  
         </>
     )
 }
