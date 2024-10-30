a6074798620b8893fa9df34cb473740a91683e01
Stephanie Natasya <tasya315@gmail.com>
2024-10-30 16:56:49 +0700

    Edit sidebar

--- components/distributor/Sidebar.jsx
+++ components/distributor/Sidebar.jsx
@@ -8,10 +8,10 @@ export default function Sidebar()
     return (
         <>
             <div className="bg-sky-300 w-60 p-4 text-white text-xl font-semibold">
-                <h3><Link href="/distributor/dashboard" className={path.startsWith("/distributor/dashboard") ? "text-black" : ""}>Dashboard</Link></h3>
-                <h3><Link href="/distributor/products" className={path.startsWith("/distributor/products") ? "text-black" : ""}>Products</Link></h3>
-                <h3><Link href="/distributor/transaction" className={path.startsWith("/distributor/transaction") ? "text-black" : ""}>Transaction</Link></h3>
-                <h3><Link href="/distributor/chat" className={path.startsWith("/distributor/chat") ? "text-black" : ""}>Chat</Link></h3>
+                <h3><Link href="/distributor" className={path == "/distributor" ? "text-black" : ""}>Dashboard</Link></h3>
+                <h3><Link href="/distributor/products" className={path == "/distributor/products" ? "text-black" : ""}>Products</Link></h3>
+                <h3><Link href="/distributor/transaction" className={path == "/distributor/transaction" ? "text-black" : ""}>Transaction</Link></h3>
+                <h3><Link href="/distributor/chat" className={path == "/distributor/chat" ? "text-black" : ""}>Chat</Link></h3>
             </div>
         </>
     )
