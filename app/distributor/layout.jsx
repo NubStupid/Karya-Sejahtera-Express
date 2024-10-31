8f6cd921026ca30843c5af69210fe111916aed25
Ferdinand Susanto Halim <119311557+NubStupid@users.noreply.github.com>
2024-10-31 21:48:46 +0700

    transaction distributor done

--- app/distributor/layout.jsx
+++ app/distributor/layout.jsx
@@ -4,11 +4,11 @@ import Sidebar from "@/components/distributor/Sidebar";
 export default function DistributorLayout({ children }) {
     return (
       <>
-        <div className="h-screen">
+        <div className="h-screen overflow-hidden">
             <Navbar />
             <div className="h-full flex">
                 <Sidebar />
-                <div className="w-full p-14">
+                <div className="w-full p-14 overflow-auto">
                     {children}
                 </div>
             </div>
