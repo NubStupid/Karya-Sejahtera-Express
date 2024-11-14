836f555bc47b47e15c581d6b94bee3ff1d030d98
Stephanie Natasya <tasya315@gmail.com>
2024-11-14 10:36:45 +0700

    Chat notif

--- components/distributor/Sidebar.jsx
+++ components/distributor/Sidebar.jsx
@@ -1,8 +1,7 @@
 "use client"
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-// import { io } from "socket.io-client";
+import { useState } from "react";
 import UnreadChat from "../UnreadChat";
 import useAuth from "@/stores/store";
 
@@ -13,49 +12,6 @@ export default function Sidebar()
 
     const [ unread, setUnread ] = useState(0)
     const [ user, setUser ] = useState({username: auth.user.username, role: auth.user.role})
-    // const [ socket, setSocket ] = useState()
-    // const fetchChat = async() => {
-        
-    //     let messages;
-    //     messages = await fetch('http://localhost:3000/api/general/chat/?username=' + user.username)
-    //     messages = await messages.json()
-    //     messages = messages.chats[0].messages
-    //     if(messages.length > 0)
-    //     {
-    //         let unread = 0;
-    //         let ctr = messages.length - 1;
-    //         while(ctr >= 0 && messages[ctr].read == false && messages[ctr].sender == "admin")
-    //         {
-    //             unread++;
-    //             ctr--;
-    //         }
-    //         setUnread(unread)
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     fetchChat();
-    // }, [])
-
-    // useEffect(() => {
-    //     let socket = io();
-        
-    //     socket.on('connect', () => {
-    //         console.log(user.username + ' connected to server');
-    //     });
-        
-    //     socket.emit('join_room', (user.username));
-
-    //     socket.on('admin_reply', (msg) => {
-    //         fetchChat();
-    //     });
-        
-    //     setSocket(socket)
-    
-    //     return () => {
-    //       socket.disconnect();
-    //     };
-    //   }, []);
 
     return (
         <>
