fa70072fa444b36f75a652f7fa6be212fc52f49f
Stephanie Natasya <tasya315@gmail.com>
2024-11-28 00:38:15 +0700

    bug fixing chat

--- app/api/general/chats/route.js
+++ app/api/general/chats/route.js
@@ -1,9 +1,12 @@
 import connectMongoDB from "@/database/connectDB";
-import Chats from "@/models/Chats";
+// import Chats from "@/models/Chats";
+import Users from "@/models/Users";
 import { NextResponse } from "next/server";
 
 export async function GET() {
     await connectMongoDB();
-    let chats = await Chats.find().sort({ updatedAt: -1 });
+    let chats = await Users.find({role: { $ne: "admin" }, "chats.updatedAt": { $ne: null }}).sort({ "chats.updatedAt": -1 });
+    // chats = chats.map((c) => (c.chats))
+    
     return NextResponse.json({chats});
 }
